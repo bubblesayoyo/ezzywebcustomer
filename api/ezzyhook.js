e76630ea747ce83550edd51ba47185d0433c09ee
bubblesayoyo <scannon8@gmail.com>
2025-07-28 17:09:39 -0400

    Update ezzyhook.js

--- api/ezzyhook.js
+++ api/ezzyhook.js
@@ -1,29 +1,9 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   console.log("🔥 METHOD:", req.method);
   console.log("🔥 HEADERS:", req.headers);
 
-  // If EzzyBills insists on GET, we just fake the response
-  const modifiedCustomer = "Saint Ignatius High School Webstores";
-
-  if (req.method === 'GET') {
-    return res.status(200).json({ Customer: modifiedCustomer });
-  }
-
-  // fallback if they ever fix it to POST
-  try {
-    const buffers = [];
-    for await (const chunk of req) {
-      buffers.push(chunk);
-    }
-    const bodyString = Buffer.concat(buffers).toString();
-    const data = JSON.parse(bodyString);
-
-    if (data.Customer && !data.Customer.endsWith(" Webstores")) {
-      data.Customer += " Webstores";
-    }
-
-    return res.status(200).json(data);
-  } catch (err) {
-    return res.status(400).json({ error: 'Invalid JSON', details: err.message });
-  }
+  // Always return a static Customer name to keep EzzyBills happy
+  return res.status(200).json({
+    Customer: "Saint Ignatius High School Webstores"
+  });
 }
