9fee3cf8eb4f7f5ab455d6a6146fefd58e8d270f
bubblesayoyo <scannon8@gmail.com>
2025-07-28 17:25:02 -0400

    Update ezzyhook.js

--- api/ezzyhook.js
+++ api/ezzyhook.js
@@ -1,15 +1,4 @@
 export default function handler(req, res) {
-  return res.status(200).json({
-    status: "success",
-    data: {
-      Customer: "Saint Ignatius High School Webstores",
-      keyword_article_code: "ST350",
-      keyword_description: "Performance Tee Full Front",
-      keyword_unit_price: 34.00,
-      keyword_total_price: 34.00,
-      charge: 6.54,
-      WorkOrder: "5449319",
-      JobName: "Kaden"
-    }
-  });
+  console.log("🔥 EzzyBills hit the webhook!");
+  return res.status(200).send("OK");
 }
