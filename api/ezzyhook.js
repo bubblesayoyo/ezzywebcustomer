34c90cdd8d9d34db960fd9c463d987a1f4b063eb
bubblesayoyo <scannon8@gmail.com>
2025-07-29 11:51:44 -0400

    Update ezzyhook.js

--- api/ezzyhook.js
+++ api/ezzyhook.js
@@ -2,6 +2,9 @@ export default function handler(req, res) {
   console.log("🔥 EzzyBills webhook called");
 
   return res.status(200).json({
-    supplier: "Saint Ignatius High School Webstores"
+    status: "success",
+    data: {
+      supplier: "Saint Ignatius High School Webstores"
+    }
   });
 }
